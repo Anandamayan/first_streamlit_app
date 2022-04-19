449813bfc3c255765173b9c3b4a08148e4d2b702
Anandamayan <36894729+Anandamayan@users.noreply.github.com>
2022-04-19 07:21:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ streamlit.title('My Name is Anand')
 streamlit.header('Everforce')
 
 streamlit.text('Data_Force & Data_gen_force')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
